f9593c26f6631f22096da75c6502ba38e1df8989
woong bin kim <woongnistelrooy@hanmail.net>
2015-07-27 09:39:31 +0900

    07_26_2

--- testing/src/BookStore/Main.java
+++ testing/src/BookStore/Main.java
@@ -1,10 +1,16 @@
 package BookStore;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String maker = null;
+		Scanner sc = new Scanner(maker);
+		
+		
+		
+		
 	}
-
 }
